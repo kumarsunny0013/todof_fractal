da18840b0d121460822ae2a7623762a854f21339
Sunny Kumar <kumarsunny0013@gmail.com>
2021-01-19 22:08:06 +0530

    header

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -5,19 +5,19 @@ export default class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <Link to="/" className="navbar-brand">ToDo App</Link>
+          <Link to="/todof_fractal" className="navbar-brand">ToDo App</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" to="/home">
+                <Link className="nav-link active" to="/todof_fractal/home">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/todof_fractal/about">About</Link>
               </li>
             </ul>
           </div>
